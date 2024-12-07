d6adef138d91329b1329be5532656122bb64e348
unknown <ramchrist20@gmail.com>
2024-12-07 16:13:37 +0800

    added backend files

---
+++ back-end/README.md
@@ -0,0 +1,18 @@
+# Back-end 
+
+---
+
+### Using:
+- Maven Apache
+- Firebase Java
+
+#### to run:
+cd to back-end
+
+```
+cd back-end
+mvn clean install -U
+mvn exec:java
+```
+
+run this on terminal only applicable to @tcker
---
+++ back-end/src/main/java/com/example/backend/Main.java
@@ -0,0 +1,8 @@
+package com.example.backend;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Firebase Backend!");
+        // Your application logic here, such as initializing Firebase and handling requests
+    }
+}
