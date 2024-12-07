4d1e60c1e1c7a2e5bb83b394082bb3c05703d0fd
unknown <ramchrist20@gmail.com>
2024-12-07 16:10:57 +0800

    initialized: Maven Apache Firebase Java as backend

---
+++ front-end/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "test-java-fiscal.firebaseapp.com",
+  projectId: "test-java-fiscal",
+  storageBucket: "test-java-fiscal.firebasestorage.app",
+  messagingSenderId: "996027067041",
+  appId: "1:996027067041:web:dddd73ba9a1e1dbfb91ee0",
+  measurementId: "G-QE50XLGR51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
